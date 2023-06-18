42af6a019baf49dc641c0c5840e87658a026f68e
shaybek <s7aybek@gmail.com>
2023-06-18 12:18:48 +0100

    fix inference.py

--- inference.py
+++ inference.py
@@ -1,4 +1,26 @@
 from dinov2.models.vision_transformer import vit_base
+import torch
+from torchvision import transforms
+from PIL import Image
+import requests
+
+
 model = vit_base()
-for name,param in model.named_parameters():
-    print(name,param.shape)
+
+#equip model with weights
+state_dict = torch.hub.load_state_dict_from_url("https://dl.fbaipublicfiles.com/dinov2/dinov2_vitb14/dinov2_vitb14_pretrain.pth",
+                                                map_location="cpu")
+
+model.load_state_dict(state_dict)
+
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+transformations=transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225])
+])
+pixel_values=transformations(image).unsqueeze(0)
+outputs=model.forward_features(pixel_values)
+print("Outputs",outputs)
